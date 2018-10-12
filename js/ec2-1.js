69987a25cd0b0fd71c8d482f23ed69c9c8896eb2
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2018-10-12 19:20:25 +0300

    volve ecuation

---
+++ js/ec2-1.js
@@ -0,0 +1,67 @@
+document.getElementById("id_buisness_version").innerHTML ="Business version: 2018.10.12.3";
+
+function read_data()
+{
+	var _a = document.getElementById("id_a").value;
+	var _b = document.getElementById("id_b").value;
+	var _c = document.getElementById("id_c").value;
+	
+	var coef = {a:_a, b:_b, c:_c}
+	return coef;
+}
+
+
+function compute_delta(coef){
+	return coelf.b * coef.b - 4 * coef.a * coef.c;
+	
+}
+
+
+
+function solve(coef)
+{
+		
+	
+	var delta = compute_delta(coef);
+	
+	var x1_re, x1_im, x2_re, x2_im;
+	
+	if(delta >= 0){
+		x1_re = (-coef.b - Math.sqrt(delta)) / (2 * coef.a);
+		x1_im=0;
+		x2_re=(-coef.b + Math.sqrt(delta)) / (2 * coef.a);
+		x2_im=0;
+		
+	}
+	else{
+		x1_re = -coef.b / (2 * coef.a);
+		x1_im = - Math.sqrt(-delta) / (2 * coef.a);
+		x2_re = -coef.b / (2 * coef.a);
+		x2_im = Math.sqrt(-delta) / (2 * coef.a);
+		
+	}
+	
+	var x1 = {re:x1_re, im:x1_im};
+	var x2 = {re:x2_re, im:x2_im};
+	var solution = {x1:x1, x2:x2};
+	return solution
+	
+}
+
+
+function print_solutions(solution)
+{
+	document.getElementById("id_x1").innerHTML = solution.x1.x1_re + " + " + solution.x1.x1_im + "i";
+	document.getElementById("id_x2").innerHTML = solution.x2.x2_re + " + " + solution.x2.x2_im + "i";
+	
+}
+
+
+
+function solve()
+{
+var coef = read_data();
+var solution = solve(coef);
+print_solutions(solution);
+	
+}
