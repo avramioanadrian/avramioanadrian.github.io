d4a7f59128ced9a9a7d2ce103a8142833d2bdf91
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2018-10-19 17:26:00 +0300

    ec2 v7

--- js/ec2-2.js
+++ js/ec2-2.js
@@ -15,13 +15,13 @@ function EC2(){
 
 	
 	if(delta >= 0){
-		this.x1 = {re:(-coef.b - Math.sqrt(delta)) / (2 * coef.a), im:0};
-		this.x2={re:(-coef.b + Math.sqrt(delta)) / (2 * coef.a), im:0};
+		this.x1 = {re:(-this.b - Math.sqrt(delta)) / (2 * this.a), im:0};
+		this.x2={re:(-this.b + Math.sqrt(delta)) / (2 * this.a), im:0};
 		
 	}
 	else{
-		this.x1 ={re:-coef.b / (2 * coef.a), im:- Math.sqrt(-delta) / (2 * coef.a)};
-		this.x2= {re:-coef.b / (2 * coef.a), im: Math.sqrt(-delta) / (2 * coef.a)};
+		this.x1 ={re:-this.b / (2 * this.a), im:- Math.sqrt(-delta) / (2 * this.a)};
+		this.x2= {re:-this.b / (2 * this.a), im: Math.sqrt(-delta) / (2 * this.a)};
 		
 	}
 	};
