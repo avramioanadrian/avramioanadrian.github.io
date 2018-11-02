5b35f349578f878afa81470612c3d827da67cb48
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2018-11-02 18:25:49 +0200

    orientare v1

--- js/orientare.js
+++ js/orientare.js
@@ -2,8 +2,8 @@ document.getElementById("id_business_version").innerHTML = "Bussiness version: 2
 window.addEventListener("deviceorientation", on_device_orinetation);
 
 function on_device_orinetation(e){
-	document.getElementById("id_x").innerHTML = e.data;
-	document.getElementById("id_y").innerHTML = e.data;
-	document.getElementById("id_z").innerHTML = e.data;
+	document.getElementById("id_x").innerHTML = e.alpha;
+	document.getElementById("id_y").innerHTML = e.beta;
+	document.getElementById("id_z").innerHTML = e.gama;
 
 }
