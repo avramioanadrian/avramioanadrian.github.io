aab9bddfcb564fc27839789a38d9002ea91cc7a4
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2018-11-02 18:59:22 +0200

    canvas

--- js/orientare.js
+++ js/orientare.js
@@ -1,9 +1,18 @@
-document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02 - 02";
+document.getElementById("id_business_version").innerHTML = "Bussiness version: 2018.11.02 - 03";
 window.addEventListener("deviceorientation", on_device_orinetation);
 
 function on_device_orinetation(e){
 	document.getElementById("id_x").innerHTML = e.alpha;
 	document.getElementById("id_y").innerHTML = e.beta;
 	document.getElementById("id_z").innerHTML = e.gamma;
+	
+	var canvas = document.getElementById('id_canvas');
+	var context = canvas.getContext('2d');
+	context.biginPath();
+	context.arc(canvas.width/2 + e.beta/90 * (canvas.width/2 - R),
+	canvas.width/2 + e.gamma/90 * (canvas.width/2 - R),
+	R,
+	0, 2*Math.PI);
+	context.stroke();
 
 }
