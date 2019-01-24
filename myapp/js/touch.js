13f49abed1fe032b096926e3707ac0d9439eb082
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2019-01-24 21:02:24 +0200

    new 1

--- myapp/js/touch.js
+++ myapp/js/touch.js
@@ -2,11 +2,11 @@ var box1 = document.getElementById('box1');
 box1.addEventListener("touchstart", on_touch_start);
 var startx = 0;
 var statusdiv = document.getElementById('statusdiv');
+var count = 0;
 function on_touch_start(e){
-	var touches = e.changedTouches;
-        var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
-        startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
-        statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px';
+	count = count + 1;
+
+        statusdiv.innerHTML = 'Counter:' + count ;
         e.preventDefault();
 	
 	
