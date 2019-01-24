70a1aec748cca2fb18ecd1f601365b9f3bbd0b20
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2019-01-24 20:02:51 +0200

    v1

--- myapp/js/touch.js
+++ myapp/js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("demo").addEventListener("touchstart", on_touch_start);
+document.getElementById("touch").addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e){
 	var touches = e.changedTouches;
@@ -7,7 +7,7 @@ function on_touch_start(e){
 	for(var i = 0 ; i< touches.length; i++){
 		var count = 0;
 
-		document.getElementById("demo").innerHTML = ++count;
+		document.getElementById("counter").innerHTML = ++count;
 		
 	}
 	
