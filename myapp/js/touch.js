b281c2803782f42ed0825013deba58ba7a499977
avramioanadrian <44093469+avramioanadrian@users.noreply.github.com>
2019-01-24 20:54:48 +0200

    new

--- myapp/js/touch.js
+++ myapp/js/touch.js
@@ -1,15 +1,18 @@
-document.getElementById("touch").addEventListener("touchstart", on_touch_start);
-
+var box1 = document.getElementById('box1');
+box1.addEventListener("touchstart", on_touch_start);
+var startx = 0;
+var statusdiv = document.getElementById('statusdiv');
 function on_touch_start(e){
 	var touches = e.changedTouches;
-
+        var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+        startx = parseInt(touchobj.clientX); // get x position of touch point relative to left edge of browser
+        statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px';
+        e.preventDefault();
 	
-	for(var i = 0 ; i< touches.length; i++){
-		var count = 0;
+	
+	
+	
 
-		document.getElementById("counter").innerHTML = ++count;
-		
-	}
 	
 	
 }
